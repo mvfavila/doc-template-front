d2a1b4e0fff2ed1c848b94b68a7788aa6730fe82
mvfavila <mvfavila@gmail.com>
2025-03-29 19:08:35 -0300

    Remove analytics temporarely

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
 
 // Initialize Firebase first
 const firebaseConfig = {
@@ -20,4 +19,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
