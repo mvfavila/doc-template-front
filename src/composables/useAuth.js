cc6204d477994e3c7181a6ae9e3ec2d5f0a1dc8a
mvfavila <mvfavila@gmail.com>
2025-03-31 20:39:03 -0300

    Add signOut func back to useAuth.js

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,5 +1,5 @@
 import { ref } from "vue";
-import { getAuth, onAuthStateChanged } from "@firebase/auth";
+import { getAuth, onAuthStateChanged, signOut } from "@firebase/auth";
 import { getFirestore, doc, getDoc } from "@firebase/firestore";
 
 export const useAuth = () => {
